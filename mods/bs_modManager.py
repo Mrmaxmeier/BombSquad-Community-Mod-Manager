b8ad5fcbcf4868063cad04bf6565340c8e38557f
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-07-19 15:06:49 +0200

    android fixes

--- mods/bs_modManager.py
+++ mods/bs_modManager.py
@@ -27,174 +27,175 @@ else:
 
 
 
-def newInit(self,transition='inRight'):
+def newInit(self, transition='inRight', originWidget=None):
+	## TODO: close animation w/ originWidget
 
-		width = 750 if gSmallUI else 580
-		height = 435
+	width = 750 if gSmallUI else 580
+	height = 435
 
-		buttonHeight = 42
+	buttonHeight = 42
 
-		R = bs.getResource('settingsWindow')
+	R = bs.getResource('settingsWindow')
 
-		topExtra = 20 if gSmallUI else 0
-		self._rootWidget = bs.containerWidget(size=(width,height+topExtra),transition=transition,
-											  scale=1.75 if gSmallUI else 1.35 if gMedUI else 1.0,
-											  stackOffset=(0,-8) if gSmallUI else (0,0))
-		self._backButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(40,height-55),size=(130,60),scale=0.8,textScale=1.2,
-							label=bs.getResource('backText'),buttonType='back',onActivateCall=self._doBack)
-		bs.containerWidget(edit=self._rootWidget,cancelButton=b)
+	topExtra = 20 if gSmallUI else 0
+	self._rootWidget = bs.containerWidget(size=(width,height+topExtra),transition=transition,
+										  scale=1.75 if gSmallUI else 1.35 if gMedUI else 1.0,
+										  stackOffset=(0,-8) if gSmallUI else (0,0))
+	self._backButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(40,height-55),size=(130,60),scale=0.8,textScale=1.2,
+						label=bs.getResource('backText'),buttonType='back',onActivateCall=self._doBack)
+	bs.containerWidget(edit=self._rootWidget,cancelButton=b)
 
-		t = bs.textWidget(parent=self._rootWidget,position=(0,height-44),size=(width,25),text=R.titleText,color=gTitleColor,
-						  hAlign="center",vAlign="center",maxWidth=130)
+	t = bs.textWidget(parent=self._rootWidget,position=(0,height-44),size=(width,25),text=R.titleText,color=gTitleColor,
+					  hAlign="center",vAlign="center",maxWidth=130)
 
-		if gDoAndroidNav:
-			bs.buttonWidget(edit=b,buttonType='backSmall',size=(60,60),label=bs.getSpecialChar('logoFlat'))
-			bs.textWidget(edit=t,hAlign='left',position=(93,height-44))
+	if gDoAndroidNav:
+		bs.buttonWidget(edit=b,buttonType='backSmall',size=(60,60),label=bs.getSpecialChar('logoFlat'))
+		bs.textWidget(edit=t,hAlign='left',position=(93,height-44))
 
-		v = height - 80
-		v -= 150
+	v = height - 80
+	v -= 150
 
-		bw = 215 if gSmallUI else 180
-		bh = 175
-		xOffs = 60 if gSmallUI else 36
-		#######NEWSTUFF########
-		xOffs2 = xOffs+bw-7
-		xOffs3 = xOffs+2*(bw-7)
-		xOffs4 = xOffs+0*(bw-7)
-		xOffs5 = xOffs+1*(bw-7)
-		xOffs6 = xOffs+2*(bw-7)
-		#######################
-		def _bTitle(x,y,button,text):
-			bs.textWidget(parent=self._rootWidget,text=text,position=(x+bw*0.47,y+bh*0.22),
-						  maxWidth=bw*0.7,size=(0,0),hAlign='center',vAlign='center',drawController=button,
-						  color=(0.7,0.9,0.7,1.0))
+	bw = 215 if gSmallUI else 180
+	bh = 175
+	xOffs = 60 if gSmallUI else 36
+	#######NEWSTUFF########
+	xOffs2 = xOffs+bw-7
+	xOffs3 = xOffs+2*(bw-7)
+	xOffs4 = xOffs+0*(bw-7)
+	xOffs5 = xOffs+1*(bw-7)
+	xOffs6 = xOffs+2*(bw-7)
+	#######################
+	def _bTitle(x,y,button,text):
+		bs.textWidget(parent=self._rootWidget,text=text,position=(x+bw*0.47,y+bh*0.22),
+					  maxWidth=bw*0.7,size=(0,0),hAlign='center',vAlign='center',drawController=button,
+					  color=(0.7,0.9,0.7,1.0))
 
-		# acb = self._accountButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs,v),size=(bw,bh),buttonType='square',
-		#                                                label='',onActivateCall=self._doAccount)
-		# _bTitle(xOffs,v,acb,R.accountText)
-		# iw = ih = 110
-		# bs.imageWidget(parent=self._rootWidget,position=(xOffs+bw*0.49-iw*0.5,v+45),size=(iw,ih),
-		#                            texture=bs.getTexture('accountIcon'),drawController=acb)
+	# acb = self._accountButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs,v),size=(bw,bh),buttonType='square',
+	#                                                label='',onActivateCall=self._doAccount)
+	# _bTitle(xOffs,v,acb,R.accountText)
+	# iw = ih = 110
+	# bs.imageWidget(parent=self._rootWidget,position=(xOffs+bw*0.49-iw*0.5,v+45),size=(iw,ih),
+	#                            texture=bs.getTexture('accountIcon'),drawController=acb)
 
-		pb = self._profilesButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs,v),size=(bw,bh),buttonType='square',
-												   label='',onActivateCall=self._doProfiles)
-		_bTitle(xOffs,v,pb,R.playerProfilesText)
-		iw = ih = 100
-		bs.imageWidget(parent=self._rootWidget,position=(xOffs+bw*0.49-iw*0.5,v+43),size=(iw,ih),
-								   texture=bs.getTexture('cuteSpaz'),drawController=pb)
+	pb = self._profilesButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs,v),size=(bw,bh),buttonType='square',
+											   label='',onActivateCall=self._doProfiles)
+	_bTitle(xOffs,v,pb,R.playerProfilesText)
+	iw = ih = 100
+	bs.imageWidget(parent=self._rootWidget,position=(xOffs+bw*0.49-iw*0.5,v+43),size=(iw,ih),
+							   texture=bs.getTexture('cuteSpaz'),drawController=pb)
 
-		cb = self._controllersButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs2,v),size=(bw,bh),buttonType='square',
-													  label='',onActivateCall=self._doControllers)
-		_bTitle(xOffs2,v,cb,R.controllersText)
-		iw = ih = 130
-		bs.imageWidget(parent=self._rootWidget,position=(xOffs2+bw*0.49-iw*0.5,v+35),size=(iw,ih),
-								   texture=bs.getTexture('controllerIcon'),drawController=cb)
+	cb = self._controllersButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs2,v),size=(bw,bh),buttonType='square',
+												  label='',onActivateCall=self._doControllers)
+	_bTitle(xOffs2,v,cb,R.controllersText)
+	iw = ih = 130
+	bs.imageWidget(parent=self._rootWidget,position=(xOffs2+bw*0.49-iw*0.5,v+35),size=(iw,ih),
+							   texture=bs.getTexture('controllerIcon'),drawController=cb)
 
-		gb = self._graphicsButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs3,v),size=(bw,bh),buttonType='square',
-												   label='',onActivateCall=self._doGraphics)
-		_bTitle(xOffs3,v,gb,R.graphicsText)
-		iw = ih = 110
-		bs.imageWidget(parent=self._rootWidget,position=(xOffs3+bw*0.49-iw*0.5,v+42),size=(iw,ih),
-								   texture=bs.getTexture('graphicsIcon'),drawController=gb)
+	gb = self._graphicsButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs3,v),size=(bw,bh),buttonType='square',
+											   label='',onActivateCall=self._doGraphics)
+	_bTitle(xOffs3,v,gb,R.graphicsText)
+	iw = ih = 110
+	bs.imageWidget(parent=self._rootWidget,position=(xOffs3+bw*0.49-iw*0.5,v+42),size=(iw,ih),
+							   texture=bs.getTexture('graphicsIcon'),drawController=gb)
 
-		v -= (bh-10)
-
-
-		ab = self._audioButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs4,v),size=(bw,bh),buttonType='square',
-												label='',onActivateCall=self._doAudio)
-		_bTitle(xOffs4,v,ab,R.audioText)
-		iw = ih = 120
-		bs.imageWidget(parent=self._rootWidget,position=(xOffs4+bw*0.49-iw*0.5+5,v+35),size=(iw,ih),
-					   color=(1,1,0),
-					   texture=bs.getTexture('audioIcon'),drawController=ab)
+	v -= (bh-10)
 
 
-		avb = self._advancedButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs5,v),size=(bw,bh),buttonType='square',
-														 label='',onActivateCall=self._doAdvanced)
-		_bTitle(xOffs5,v,avb,R.advancedText)
-		iw = ih = 120
-		bs.imageWidget(parent=self._rootWidget,position=(xOffs5+bw*0.49-iw*0.5+5,v+35),size=(iw,ih),
-					   color=(0.8,0.95,1),
-					   texture=bs.getTexture('advancedIcon'),drawController=avb)
+	ab = self._audioButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs4,v),size=(bw,bh),buttonType='square',
+											label='',onActivateCall=self._doAudio)
+	_bTitle(xOffs4,v,ab,R.audioText)
+	iw = ih = 120
+	bs.imageWidget(parent=self._rootWidget,position=(xOffs4+bw*0.49-iw*0.5+5,v+35),size=(iw,ih),
+				   color=(1,1,0),
+				   texture=bs.getTexture('audioIcon'),drawController=ab)
+
+
+	avb = self._advancedButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs5,v),size=(bw,bh),buttonType='square',
+													 label='',onActivateCall=self._doAdvanced)
+	_bTitle(xOffs5,v,avb,R.advancedText)
+	iw = ih = 120
+	bs.imageWidget(parent=self._rootWidget,position=(xOffs5+bw*0.49-iw*0.5+5,v+35),size=(iw,ih),
+				   color=(0.8,0.95,1),
+				   texture=bs.getTexture('advancedIcon'),drawController=avb)
 
 
 
-		####NEWSTUFF####
-		mb = self._modManagerButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs6,v),size=(bw,bh),buttonType='square',
-														 label='',onActivateCall=self._doModManager)
-		_bTitle(xOffs6,v,mb, 'Mod Manager')
-		iw = ih = 120
-		bs.imageWidget(parent=self._rootWidget,position=(xOffs6+bw*0.49-iw*0.5+5,v+35),size=(iw,ih),
-					   color=(0.8,0.95,1),
-					   texture=bs.getTexture('heart'),drawController=mb)
-		################
+	####NEWSTUFF####
+	mb = self._modManagerButton = b = bs.buttonWidget(parent=self._rootWidget,autoSelect=True,position=(xOffs6,v),size=(bw,bh),buttonType='square',
+													 label='',onActivateCall=self._doModManager)
+	_bTitle(xOffs6,v,mb, 'Mod Manager')
+	iw = ih = 120
+	bs.imageWidget(parent=self._rootWidget,position=(xOffs6+bw*0.49-iw*0.5+5,v+35),size=(iw,ih),
+				   color=(0.8,0.95,1),
+				   texture=bs.getTexture('heart'),drawController=mb)
+	################
 
-		# self._profilesButton = b = bs.buttonWidget(parent=self._rootWidget,position=(53,v),size=(width-110,buttonHeight),
-		#                                            label=R.playerProfilesText,onActivateCall=self._doProfiles)
-		# v -= spacing * 1.6
-		# self._controllersButton = b = bs.buttonWidget(parent=self._rootWidget,position=(61,v),size=(width-110,buttonHeight),
-		#                                               label=R.controllersText,onActivateCall=self._doControllers)
-		# v -= spacing * 1.6
+	# self._profilesButton = b = bs.buttonWidget(parent=self._rootWidget,position=(53,v),size=(width-110,buttonHeight),
+	#                                            label=R.playerProfilesText,onActivateCall=self._doProfiles)
+	# v -= spacing * 1.6
+	# self._controllersButton = b = bs.buttonWidget(parent=self._rootWidget,position=(61,v),size=(width-110,buttonHeight),
+	#                                               label=R.controllersText,onActivateCall=self._doControllers)
+	# v -= spacing * 1.6
 
-		# self._graphicsButton = b = bs.buttonWidget(parent=self._rootWidget,position=(51,v),size=(width-110,buttonHeight),
-		#                                            label=R.graphicsText,onActivateCall=self._doGraphics)
-		# v -= spacing * 1.6
-		# self._audioButton = b = bs.buttonWidget(parent=self._rootWidget,position=(57,v),size=(width-110,buttonHeight),
-		#                                         label=R.audioText,onActivateCall=self._doAudio)
-		# v -= spacing * 1.4
-		# avb = self._advancedButton = b = bs.buttonWidget(parent=self._rootWidget,position=(59,v),size=(width-110,buttonHeight),
-		#                                                  label=R.advancedText,onActivateCall=self._doAdvanced,
-		#                                                  color=(0.55,0.5,0.6),
-		#                                                  textColor=(0.65,0.6,0.7),
-		# )
+	# self._graphicsButton = b = bs.buttonWidget(parent=self._rootWidget,position=(51,v),size=(width-110,buttonHeight),
+	#                                            label=R.graphicsText,onActivateCall=self._doGraphics)
+	# v -= spacing * 1.6
+	# self._audioButton = b = bs.buttonWidget(parent=self._rootWidget,position=(57,v),size=(width-110,buttonHeight),
+	#                                         label=R.audioText,onActivateCall=self._doAudio)
+	# v -= spacing * 1.4
+	# avb = self._advancedButton = b = bs.buttonWidget(parent=self._rootWidget,position=(59,v),size=(width-110,buttonHeight),
+	#                                                  label=R.advancedText,onActivateCall=self._doAdvanced,
+	#                                                  color=(0.55,0.5,0.6),
+	#                                                  textColor=(0.65,0.6,0.7),
+	# )
 
-		# bs.buttonWidget(edit=acb,downWidget=gb)
-		# bs.buttonWidget(edit=pb,downWidget=ab,upWidget=self._backButton)
-		# bs.buttonWidget(edit=cb,downWidget=avb,upWidget=self._backButton)
-		# bs.buttonWidget(edit=gb,upWidget=acb)
-		# bs.buttonWidget(edit=ab,upWidget=pb)
-		# bs.buttonWidget(edit=avb,upWidget=cb)
-		# bs.buttonWidget(edit=cb,downWidget=ab)
-		# bs.buttonWidget(edit=gb,downWidget=avb,upWidget=pb)
-		# bs.buttonWidget(edit=ab,downWidget=avb,upWidget=cb)
-		# bs.buttonWidget(edit=avb,upWidget=gb)
+	# bs.buttonWidget(edit=acb,downWidget=gb)
+	# bs.buttonWidget(edit=pb,downWidget=ab,upWidget=self._backButton)
+	# bs.buttonWidget(edit=cb,downWidget=avb,upWidget=self._backButton)
+	# bs.buttonWidget(edit=gb,upWidget=acb)
+	# bs.buttonWidget(edit=ab,upWidget=pb)
+	# bs.buttonWidget(edit=avb,upWidget=cb)
+	# bs.buttonWidget(edit=cb,downWidget=ab)
+	# bs.buttonWidget(edit=gb,downWidget=avb,upWidget=pb)
+	# bs.buttonWidget(edit=ab,downWidget=avb,upWidget=cb)
+	# bs.buttonWidget(edit=avb,upWidget=gb)
 
-		# if 0:
-		#     v -= spacing * 1.57
-		#     configCheckBox(parent=self._rootWidget,position=(60,v),size=(width-100,30),name="Show Player Names",displayName=R.showPlayerNamesText,scale=0.9)
-		#     v -= spacing * 1.27
-		#     configCheckBox(parent=self._rootWidget,position=(60,v),size=(width-100,30),name="Kick Idle Players",displayName=R.kickIdlePlayersText,scale=0.9)
-		#     v -= spacing * 0.63
+	# if 0:
+	#     v -= spacing * 1.57
+	#     configCheckBox(parent=self._rootWidget,position=(60,v),size=(width-100,30),name="Show Player Names",displayName=R.showPlayerNamesText,scale=0.9)
+	#     v -= spacing * 1.27
+	#     configCheckBox(parent=self._rootWidget,position=(60,v),size=(width-100,30),name="Kick Idle Players",displayName=R.kickIdlePlayersText,scale=0.9)
+	#     v -= spacing * 0.63
 
-		#     thisButtonWidth = 140
-		#     b = bs.buttonWidget(parent=self._rootWidget,position=(width/2-thisButtonWidth/2,v-14),
-		#                         color=(0.45,0.4,0.5),
-		#                         size=(thisButtonWidth,22),
-		#                         label=R.enterPromoCodeText,
-		#                         textColor=(0.55,0.5,0.6),
-		#                         textScale=0.7,
-		#                         onActivateCall=PromoCodeWindow)
+	#     thisButtonWidth = 140
+	#     b = bs.buttonWidget(parent=self._rootWidget,position=(width/2-thisButtonWidth/2,v-14),
+	#                         color=(0.45,0.4,0.5),
+	#                         size=(thisButtonWidth,22),
+	#                         label=R.enterPromoCodeText,
+	#                         textColor=(0.55,0.5,0.6),
+	#                         textScale=0.7,
+	#                         onActivateCall=PromoCodeWindow)
 
 
-		self._restoreState()
+	self._restoreState()
 
-		# re-select previous if applicable
-		# selected = None
-		# try:
-		#     global gSettingsSelection
-		#     try: selName = gSettingsSelection
-		#     except Exception: selName = ''
-		#     if selName == 'Account': selected = self._accountButton
-		#     elif selName == 'Profiles': selected = self._profilesButton
-		#     elif selName == 'Controllers': selected = self._controllersButton
-		#     elif selName == 'Graphics': selected = self._graphicsButton
-		#     elif selName == 'Audio': selected = self._audioButton
-		#     elif selName == 'Advanced': selected = self._advancedButton
-		# except Exception,ex:
-		#     print 'Exception restoring settings UI state:',ex
+	# re-select previous if applicable
+	# selected = None
+	# try:
+	#     global gSettingsSelection
+	#     try: selName = gSettingsSelection
+	#     except Exception: selName = ''
+	#     if selName == 'Account': selected = self._accountButton
+	#     elif selName == 'Profiles': selected = self._profilesButton
+	#     elif selName == 'Controllers': selected = self._controllersButton
+	#     elif selName == 'Graphics': selected = self._graphicsButton
+	#     elif selName == 'Audio': selected = self._audioButton
+	#     elif selName == 'Advanced': selected = self._advancedButton
+	# except Exception,ex:
+	#     print 'Exception restoring settings UI state:',ex
 
-		# if selected is not None: bs.containerWidget(edit=self._rootWidget,selectedChild=selected)
-		# else: bs.containerWidget(edit=self._rootWidget,selectedChild=self._accountButton)
+	# if selected is not None: bs.containerWidget(edit=self._rootWidget,selectedChild=selected)
+	# else: bs.containerWidget(edit=self._rootWidget,selectedChild=self._accountButton)
 
 oldInit = SettingsWindow.__init__
 SettingsWindow.__init__ = newInit
