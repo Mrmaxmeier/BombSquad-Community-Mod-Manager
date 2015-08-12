0b30f50d96d76026779d174fefd2d7fc95705df3
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-08-12 09:54:53 +0200

    use git to get md5, autocommit index.json

---
+++ update_index.py
@@ -0,0 +1,95 @@
+import os
+import os.path
+import json
+import hashlib
+import git
+
+gitRepo = git.Repo("./")
+
+mods = {}
+
+current_commit = gitRepo.rev_parse("HEAD")
+
+url_base = "https://cdn.rawgit.com/Mrmaxmeier/BombSquad-Community-Mod-Manager/"
+modurl = url_base + current_commit.hexsha + "/mods/"
+
+old_data = None
+
+for blob in gitRepo.head.object.tree.traverse():
+	if blob.path.startswith("mods/") and blob.path.endswith(".py"):
+		filename = blob.path[5:]
+		base = filename[:-3]
+		data = blob.data_stream.read()
+		md5 = hashlib.md5(data).hexdigest()
+		mod = {"changelog": [], "md5": md5,
+			   "url": modurl + filename,
+			   "filename": filename}
+		if os.path.isfile("mods/" + base + ".json"):
+			with open("mods/" + base + ".json", "r") as json_file:
+				mod.update(json.load(json_file))
+		mods[base] = mod
+	elif blob.path == "index.json":
+		old_data = json.loads(blob.data_stream.read().decode("UTF-8"))
+
+specific_sha = set()
+
+for commit in gitRepo.iter_commits(max_count=1000, paths="mods/"):
+	for filename in commit.stats.files:
+		if filename.startswith("mods/"):
+			filename = filename[5:]
+			if filename.endswith(".py"):
+				if not filename[:-3] in mods:
+					continue
+				txt = commit.message
+				txt = txt.replace("\n", "")
+				mods[filename[:-3]]["changelog"].append(txt)
+				if not filename in specific_sha:
+					mods[filename[:-3]]["url"] = url_base + commit.hexsha + "/mods/" + filename
+					specific_sha.add(filename)
+
+for mod in mods:
+	if not mod + ".py" in specific_sha:
+		print("didnt find latest commit for", mod + ", head is used")
+
+for mod in mods.values():
+	mod["changelog"] = mod["changelog"][:2]
+
+with open("index.json", "w") as f:
+	json.dump(mods, f, indent=4, sort_keys=True)
+
+if old_data:
+	text = ""
+	def add(text, mod, spacer, *args):
+		if spacer:
+			text += " " * (spacer + 2)
+		else:
+			text += mod + ": "
+			spacer = len(mod)
+		text += " ".join(args) + "\n"
+		return text, spacer
+	spacer = None
+	for mod in set(list(old_data.keys()) + list(mods.keys())):
+		if spacer:
+			spacer = None
+			text += "\n"
+		if mod in mods and mod in old_data:
+			md, omd = mods[mod], old_data[mod]
+			for key in set(list(md.keys()) + list(omd.keys())):
+				if key in md and key in omd:
+					if md[key] != omd[key]:
+						text, spacer = add(text, mod, spacer, 'updated', key)
+				elif not key in md:
+					text, spacer = add(text, mod, spacer, 'added', key)
+				else:
+					text, spacer = add(text, mod, spacer, 'removed', key)
+		elif mod in mods:
+			text, spacer = add(text, mod, spacer, 'added')
+		else:
+			text, spacer = add(text, mod, spacer, 'removed')
+
+	if len(text) == 0:
+		print("no changes.")
+	else:
+		text = "update index.json\n\n" + text
+		gitRepo.index.add(["index.json"])
+		gitRepo.index.commit(text)
