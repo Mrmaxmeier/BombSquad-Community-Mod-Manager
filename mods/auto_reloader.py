cab52f7838d9430e193729fc2588cef53ca32de4
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-08-28 14:25:04 +0200

    support for "patching" mods

--- mods/auto_reloader.py
+++ mods/auto_reloader.py
@@ -12,6 +12,7 @@ sys.path.append(IMPORT_FOLDER) # FIXME
 
 class GameWrapper(object):
     _game = None
+    _type = None
     _instances = weakref.WeakSet()
     def __init__(self, filename):
         self._filename = filename
@@ -19,7 +20,7 @@ class GameWrapper(object):
             self._module_md5 = md5(f.read()).hexdigest()
         self._did_print_error = False
         self._import_module()
-        if self._is_available():
+        if self._is_available() and self._type == "game":
             self._game = self._module.bsGetGames()[0]
         else:
             self._game = None
@@ -29,7 +30,8 @@ class GameWrapper(object):
             data = imp.find_module(self._filename[:-3])
             self._module = imp.load_module(self._filename[:-3], *data)
         except Exception, e:
-            print(e)
+            import traceback
+            traceback.print_exc()
             self._module = None
             self._game = None
             self._module_error(str(e))
@@ -46,15 +48,21 @@ class GameWrapper(object):
         if not hasattr(self._module, '_supports_auto_reloading'):
             self._module_error('missing _supports_auto_reloading')
             return False
+        if not hasattr(self._module, '_auto_reloader_type'):
+            self._module_error('missing _auto_reloader_type')
+            return False
+        self._type = self._module._auto_reloader_type
+
         if not hasattr(self._module, '_prepare_reload'):
             self._module_error('missing _prepare_reload')
             return False
         if not hasattr(self._module, 'bsGetAPIVersion'):
             self._module_error('missing bsGetAPIVersion')
             return False
-        if not hasattr(self._module, 'bsGetGames'):
+        if self._type == "game" and not hasattr(self._module, 'bsGetGames'):
             self._module_error('missing bsGetGames')
             return False
+
 
         if not self._module._supports_auto_reloading:
             self._module_error('doesnt support auto reloading')
@@ -62,7 +70,7 @@ class GameWrapper(object):
         if not self._module.bsGetAPIVersion() == 3:
             self._module_error('missing wrong API Version', self._module.bsGetAPIVersion())
             return False
-        if len(self._module.bsGetGames()) != 1:
+        if self._type == "game" and len(self._module.bsGetGames()) != 1:
             self._module_error("more than 1 game isnt supported") # FIXME
             return False
         return True
@@ -82,7 +90,7 @@ class GameWrapper(object):
         with open(IMPORT_FOLDER + self._filename, "r") as f:
             self._module_md5 = md5(f.read()).hexdigest()
         self._did_print_error = False
-        if self._is_available():
+        if self._is_available() and self._type == "game":
             self._game = self._module.bsGetGames()[0]
         else:
             self._game = None
@@ -95,6 +103,8 @@ class GameWrapper(object):
                 self._reload_module()
 
     def __call__(self, *args, **kwargs):
+        if not self._type == "game":
+            self._module_error("non games can't be called")
         instance = self._game(*args, **kwargs)
         self._instances.add(instance)
         return instance
@@ -130,4 +140,4 @@ def bsGetAPIVersion():
 	return 3
 
 def bsGetGames():
-	return wrappers
+	return [wrapper for wrapper in wrappers if wrapper._type == "game"]
